a978e8c3e4e8367c65d9903873f0914d2f4dca02
Joao_Gularte <107907952+OndeJoaoGularte@users.noreply.github.com>
2026-08-24 12:18:36 -0300

    Como Atuamos finalizada
    Implementação do componente Timeline e Projects

--- src/app/features/como-atuamos/components/atuamos-projects/atuamos-projects.ts
+++ src/app/features/como-atuamos/components/atuamos-projects/atuamos-projects.ts
@@ -1,9 +1,95 @@
-import { Component } from '@angular/core';
+import { Component, effect, OnDestroy, OnInit, signal } from '@angular/core';
+import { debounceTime, distinctUntilChanged, Subject, Subscription } from 'rxjs';
+import { ProjectsService } from '../../../../core/services/projects/projects';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../../../core/services/auth/auth';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-atuamos-projects',
-  imports: [],
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './atuamos-projects.html',
   styleUrl: './atuamos-projects.scss',
 })
-export class AtuamosProjectsComponent {}
+export class AtuamosProjectsComponent implements OnInit, OnDestroy {
+  projects = signal<any[]>([]);
+  loading = signal<boolean>(true);
+  searchTool: string = '';
+
+  filterActive: 'all' | boolean = 'all';
+  presentOrdination: string = 'name-asc';
+
+  private searchSubject = new Subject<string>();
+  private searchSubscription!: Subscription;
+
+  constructor(
+    private projectsService: ProjectsService,
+    private router: Router,
+    public authService: AuthService
+  ) {
+    effect(() => {
+      this.authService.currentUser(); 
+      
+      this.loadProjects(); 
+    });
+  }
+
+  ngOnInit(): void {
+    this.searchSubscription = this.searchSubject.pipe(
+      debounceTime(300),
+      distinctUntilChanged()
+    ).subscribe(term => {
+      this.searchTool = term;
+      this.loadProjects();
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.searchSubscription.unsubscribe();
+  }
+
+  onSearchInput(term: string): void {
+    this.searchSubject.next(term);
+  }
+
+ async loadProjects(): Promise<void> {
+    this.loading.set(true);
+    const isAdmin = this.authService.isLoggedIn();
+    
+    try {
+      const data = await this.projectsService.getProjects(
+        this.filterActive,
+        this.presentOrdination,
+        this.searchTool,
+        isAdmin
+      );
+      this.projects.set(data);
+    } catch (error) {
+      console.error("Erro ao carregar projetos:", error);
+    } finally {
+      this.loading.set(false);
+    }
+  }
+
+  filterByActive(active: 'all' | boolean): void {
+    this.filterActive = active;
+    this.loadProjects();
+  }
+
+  showDetails(id: number): void {
+    this.router.navigate(['/project', id]);
+  }
+
+  async deleteProject(id: number, event: MouseEvent): Promise<void> {
+    event.stopPropagation();
+    if (confirm('Tem certeza que deseja deletar este projeto? Esta ação não pode ser desfeita.')) {
+      const { error } = await this.projectsService.deleteProject(id);
+      if (!error) {
+        this.projects.update(projs => projs.filter(p => p.id !== id));
+      } else {
+        alert('Ocorreu um erro ao deletar o projeto.');
+      }
+    }
+  }
+}
--- src/app/features/como-atuamos/components/atuamos-timeline/atuamos-timeline.ts
+++ src/app/features/como-atuamos/components/atuamos-timeline/atuamos-timeline.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-atuamos-timeline',
@@ -6,4 +6,24 @@ import { Component } from '@angular/core';
   templateUrl: './atuamos-timeline.html',
   styleUrl: './atuamos-timeline.scss',
 })
-export class AtuamosTimelineComponent {}
+export class AtuamosTimelineComponent {
+  
+  timelineSteps = signal([
+    {
+      step: 1,
+      title: 'QG Humanitário',
+      description: 'Nosso QG na Av. São Pedro se tornou um centro de resgate e distribuição de doações para o 4º Distrito.'
+    },
+    {
+      step: 2,
+      title: 'Expansão da Ajuda',
+      description: 'Levamos nosso apoio a locais com menor visibilidade, como Eldorado do Sul e as Ilhas de Porto Alegre.'
+    },
+    {
+      step: 3,
+      title: 'Institucionalização',
+      description: 'Para organizar e ampliar o apoio, o grupo de voluntários se formalizou, criando o Instituto AbraceRS.'
+    }
+  ]);
+
+}
